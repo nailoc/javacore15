e941df1929091e000c73d150ec77cfa605b15f01
Administrator <Administrator@801-t>
2021-04-21 18:12:16 +0900

    javacore15 예제풀이

--- javacore15/src/com/hk/app/Exam4.java
+++ javacore15/src/com/hk/app/Exam4.java
@@ -1,5 +1,6 @@
 package com.hk.app;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -9,11 +10,23 @@ public class Exam4 {
 		
 		List num = new LinkedList();
 		
-		// 1부터 100까지의 랜덤하게 값을 저장한다
+		// 1부터 100까지의 랜덤하게 값을 저장한다 (20번)
+		for(int i=0; i<20; i++) {
+			int number = (int)(Math.random()*100+1);
+			num.add(number);
+		}
 		
 		// 반복자 Iterator 사용해서 짝수만 찾아서 출력하시오 
-		
+		Iterator itr = num.iterator();
+		int sum = 0;
+		while(itr.hasNext()) {
+			int temp = (int)itr.next();
+			if(temp%2==0) {
+				sum+=temp;
+			}
+		}
 		// 짝수의 합을 출력하시오
+		System.out.println("짝수들의 합은:"+sum);
 
 	}
 
--- javacore15/src/com/hk/app/Exam5.java
+++ javacore15/src/com/hk/app/Exam5.java
@@ -1,7 +1,10 @@
 package com.hk.app;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Exam5 {
 
@@ -9,8 +12,44 @@ public class Exam5 {
 		
 		// 전화번호를 저장한다
 		Map phone = new HashMap(); // 키, 값으로 저장
+		Scanner reader = new Scanner(System.in);
+		String start = null;
+		while(true) {
+			System.out.print("전화번호를 입력하겠습니까(y/n)?");			
+			start = reader.nextLine();
+			
+			if(start.equalsIgnoreCase("y")) {
+			
+				System.out.print("이름입력>>");
+				String name = reader.nextLine();
+			
+				System.out.print("전화번호입력>>");
+				String phnum = reader.nextLine();
+			
+				phone.put(phnum, name);
+			}else {
+				break;
+			}
+			
+		}
+		// 정렬(key기준) 
+		TreeMap tm = new TreeMap(phone);
 		
-	
+		System.out.println("저장된 정보를 출력합니다");
+		/*
+		Iterator key = phone.keySet().iterator();
+		while(key.hasNext()) {
+			String keyVal = (String)key.next();
+			System.out.println("전화번호:"+keyVal+" 이름:"+phone.get(keyVal));			
+		}
+		*/
+		//Iterator key2 = tm.keySet().iterator(); 	// 오름차순
+		Iterator key2 = tm.descendingKeySet().iterator(); // 내림차순
+		
+		while(key2.hasNext()) {
+			String keyVal2 = (String)key2.next();
+			System.out.println("전화번호:"+keyVal2+" 이름:"+tm.get(keyVal2));
+		}
 
 	}
 
